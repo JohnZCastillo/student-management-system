ca582eb9412d53d71ad8b2b3136c74522f9986e1
John Z Castillo <johnzunigacastillo@gmail.com>
2022-02-16 16:45:11 +0800

    initilize

---
+++ Global/Person.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Person.java
+ *
+ *
+ * @author 
+ * @version 1.00 2022/2/16
+ */
+
+
+public class Person {
+
+    public Person() {
+    }
+    
+    
+}
---
+++ Main.java
@@ -0,0 +1,16 @@
+/**
+ * @(#)Main.java
+ *
+ *
+ * @author 
+ * @version 1.00 2022/2/16
+ */
+
+
+public class Main {
+
+    public Main() {
+    }
+    
+    
+}
